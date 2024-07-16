4c273fea3a2b2d0f99a469ad957144142c6f3cfe
chevyboys <volcanicchevy@gmail.com>
2024-07-16 01:50:14 -0600

    chore: Update package version to 0.1.5 and add ModuleManager and EventHandler classes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,5 @@
 export * from "./Classes/DominionClient";
 export * from "./Classes/DominionConfig";
 export * from "./Classes/Module";
+export * from "./Classes/ModuleManager";
+export * from "./Classes/EventHandler";
